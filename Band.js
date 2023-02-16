7dc76669a6355a69bad20db065ac3086a67f217f
DanJackson02 <dannyjjackson2002@gmail.com>
2023-02-16 11:36:00 +0000

    Fleshed out db.js, musician.js and bands.js

--- Band.js
+++ Band.js
@@ -2,7 +2,14 @@ const {Sequelize, sequelize} = require('./db');
 
 // TODO - define the Band model
 let Band;
+//  - The `Band` model should have name and genre properties, both of which are strings.
+const sequelize = require("./db"); // Importing my connection from db.js
+const { DataTypes } = require("sequelize");
 
+const Restaurant = sequelize.define("Band", {
+  name: DataTypes.STRING,
+  genre: DataTypes.STRING,
+});
 module.exports = {
     Band
 };
--- Musician.js
+++ Musician.js
@@ -1,8 +1,15 @@
 const {Sequelize, sequelize} = require('./db');
 
 // TODO - define the Musician model
-let Musician;
+//  - The `Musician` model should have name and instrument properties, both of which are strings.
 
+const sequelize = require("./db"); // Importing my connection from db.js
+const { DataTypes } = require("sequelize");
+
+const Musician = sequelize.define("Musician", {
+  name: DataTypes.STRING,
+  instrument: DataTypes.STRING,
+});
 module.exports = {
     Musician
 };
